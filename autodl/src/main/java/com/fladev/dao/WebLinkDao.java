b297e56b3b35cba74240975a86dc3da1afab65cc
Fabrice <Fabrice@192.168.1.13>
2017-11-17 19:41:51 +0100

    Upload of the initial maven project

---
+++ autodl/src/main/java/com/fladev/dao/WebLinkDao.java
@@ -0,0 +1,7 @@
+package com.fladev.dao;
+
+import java.net.URL;
+
+public interface WebLinkDao {
+	public String getWebPageHttp(URL url);
+}
---
+++ autodl/src/main/java/com/fladev/service/BsService.java
@@ -0,0 +1,5 @@
+package com.fladev.service;
+
+public class BsService {
+
+}
